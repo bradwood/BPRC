2b67c8eea3249b83a0c23b14b6c5aaaed2e2a5d7
Bradley Wood <git@bradleywood.com>
2016-07-26 23:17:11 +0100

    more tests

--- test/test_recipe_deserialise.py
+++ test/test_recipe_deserialise.py
@@ -3,6 +3,9 @@ import yaml
 
 from bprc.recipe import Recipe
 
+#TODO: Parametrise test cases -- try this http://pastebin.com/rdMqXc7b
+
+
 class RecipeTest(unittest.TestCase):
     def setUp(self):
          """Sets up the YAML file."""
@@ -13,10 +16,10 @@ class RecipeTest(unittest.TestCase):
             datamap=yaml.safe_load(stream)
         r = Recipe(datamap)
         self.assertIsInstance(r,Recipe)
-
-
-
-
+        self.assertEquals(r.steps[0].request.headers["Authorisation"],"yadda")
+        self.assertEquals(r.steps[0].name,"Create Kong API")
+        self.assertIsNone(r.steps[0].response.body["id"]) #is None, but defined in the Yaml.
+        self.assertIsNone(r.steps[1].response.headers["Authorisation"])
 
 
 
