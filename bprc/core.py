47248f93aa898decd839903279dbe1bff70cd816
Bradley Wood <git@bradleywood.com>
2016-07-27 16:38:15 +0100

    got basic php-style substitution working in the step URL

--- bprc/core.py
+++ bprc/core.py
@@ -12,8 +12,9 @@ Invocation flow:
 """
 
 import yaml
-from recipe import Recipe
 import logging
+from recipe import Recipe
+from stepprocessor import StepProcessor
 
 def main():
     """
@@ -24,7 +25,7 @@ def main():
     """
     #configure login
     #TODO: generalise this and make logging better
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.DEBUG, format='%(levelname)s:%(asctime)s:%(message)s')
     logging.debug('Initialising log')
 
     with open("examples/recipe.yml") as stream:
@@ -36,13 +37,21 @@ def main():
 
     r = Recipe(datamap)
 
-    logging.debug(r.steps[0].request.headers["Authorisation"])
-    logging.debug(r.steps[1].request.headers["blah"])
-    logging.debug(r.steps[0].response.body["id"])
-    logging.debug(r.steps[1].response.headers["Authorisation"])
+    logging.debug('r.steps[0].request.headers["Authorisation"]=%s', r.steps[0].request.headers["Authorisation"])
+    logging.debug('r.steps[1].request.headers["blah"]=%s',r.steps[1].request.headers["blah"])
+    logging.debug('r.steps[0].response.body["id"]=%s',r.steps[0].response.body["id"])
+    logging.debug('r.steps[1].response.headers["Authorisation"]=%s',r.steps[1].response.headers["Authorisation"])
 
-
+    #loop through steps and execute each one.
+    for i, step in enumerate(r.steps):
+        logging.debug('In steps loop PREPROCESS: r.steps[%s].URL=%s',i,r.steps[i].URL)
+        # firstly, for the step about to be executed, substitute any items in the request object using the php-like
+        # <%= =>
+        processor = StepProcessor(recipe=r, stepid=i) # instantiate a step processor
+        r.steps[i] = processor.prepare() # substitutes and php-like strings to prepare the step for calling & returns
+        processor.call() # makes the call and populates the response object.
+        logging.debug('In steps loop POSTPROCESS: r.steps[%s].URL=%s',i,r.steps[i].URL)
 
     print("\n\n")
     print(r)
-
+    logging.debug('AFTER PROCESSING LOOP: r.steps[1].URL=%s',r.steps[1].URL)
---
+++ bprc/stepprocessor.py
@@ -0,0 +1,56 @@
+"""
+This module implements the logic to process a step in a recipe
+"""
+import logging
+import json
+import requests
+import re
+from functools import partial
+
+class StepProcessor():
+    """Class to process """
+
+    def __init__(self, *, recipe,stepid): #kwargs for recipe and stepid
+        """Instantiates the Step Processor Object"""
+        self.recipe = recipe
+        self.stepid = stepid
+
+    def prepare(self):
+        """prepares the step by substituting php-like constructs in the step subtree of the passed recipe"""
+        logging.debug('In StepProcessor.prepare(): stepid=%s',self.stepid)
+        def _insert_param(m, recipe):
+            """
+            used by the re.subn call below
+            takes an re.match object
+            returns a string
+            """
+            logging.debug('In StepProcessor.prepare()._insert_param: m.group(1)=%s',m.group(1))
+            logging.debug('In StepProcessor.prepare()._insert_param: recipe.steps[0].response.body["id"]=%s',recipe.steps[0].response.body["id"])
+
+            return eval('recipe.' + m.group(1))
+
+        if self.stepid == 0:
+            return self.recipe.steps[self.stepid] # nothing to process if we are on the first step TODO: maybe do some step validation later?
+        else:
+            # Algorithm:
+            # process substitutions in URL string
+            u=self.recipe.steps[self.stepid].URL
+            subpat=re.compile(r'<%=(\S+)%>') #substitution pattern to find - of the form <%=some.var["blah"]%>
+            substituted_text, n = subpat.subn(partial(_insert_param, recipe=self.recipe), u)
+            self.recipe.steps[self.stepid].URL=substituted_text
+
+            logging.debug('In StepProcessor.prepare(): substituted_text=%s',substituted_text)
+            #matches=re.search(u)
+
+            # loop through all QueryString dictionaries and process substition
+            #for key, value in self.recipe[self.stepid]
+
+            # loop through all Request.body dictionaries and process substitutions
+            # loop throuhg all Request.header dictionaries and process substitutions
+
+        return self.recipe.steps[self.stepid]
+
+    def call(self):
+        """calls the URL specified in the current step"""
+        pass
+
