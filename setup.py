5d1050e1cb7a898a660d13dbcd331a751ac1ee7c
Bradley Wood <git@bradleywood.com>
2016-08-01 12:12:56 +0100

    misc tweaks t o setup.py

--- setup.py
+++ setup.py
@@ -101,7 +101,8 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['pyyaml', 'requests'],
+    install_requires=['pyyaml>=3.11',
+                      'requests>=2.10.0'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
@@ -128,9 +129,9 @@ setup(
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
-    # entry_points={
-    #     'console_scripts': [
-    #         'sample=sample:main',
-    #     ],
-    # },
+    entry_points={
+        'console_scripts': [
+            'bprc=bprc:__main__:main',
+        ],
+    },
 )
