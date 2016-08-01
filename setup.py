1a1618a8fe3834ff099a537887147e8938d3139d
Bradley Wood <git@bradleywood.com>
2016-08-01 10:28:16 +0100

    fixed setup.py

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['pyyaml, requests'],
+    install_requires=['pyyaml', 'requests'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
